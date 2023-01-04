62c7023383590e0c4ddf378e88e89e8dd85c9f33
duanxinyi3 <duanxinyi3@jd.com>
2023-01-04 18:26:56 +0800

    feat: 20220104 姐终于分解做出来二叉树了

--- tree.js
+++ tree.js
@@ -45,6 +45,7 @@ var maxDepth = function(root) {
 };
 
 // TODO：✅==========================================================
+// 二分
 // 108. 将有序数组转换为二叉搜索树
 
 var no108_sortedArrayToBST = function(nums) {
@@ -112,30 +113,6 @@ var no113_pathSum = function(root, targetSum) {
 };
 
 // TODO：✅==========================================================
-// 114. 二叉树展开为链表
-
-var no114_flatten = function(root) {
-  const helper = (root) => {       // 将当前子树转成一个单链表
-    if (root == null) return null; // 遍历到null节点 返回null节点
-    if (root.right) {              // 先生成右子树的单链表
-      helper(root.right);
-    }
-    if (root.left) {               // 如果有左子树，生成单链表然后搬运过去
-      const leftFirst = helper(root.left); // 生成单链表，并获取头结点
-      let leftLast = leftFirst;    // leftEnd是单链表的尾节点
-      while (leftLast.right) {     // 一直找右节点，获取到单链表的尾节点
-        leftLast = leftLast.right;
-      }
-      leftLast.right = root.right; // 尾节点后面接左子树展平后的单链表
-      root.right = leftFirst;      // 根节点的right改成leftFirst
-      root.left = null;            // root.left置为null
-    }
-    return root; // 返回出当前子树转成的单链表
-  };
-  helper(root);  // 原地修改，不用返回
-};
-
-// TODO：✅==========================================================
 // 后序
 // 124. 二叉树中的最大路径和
 
@@ -158,7 +135,7 @@ var no124_maxPathSum = function(root) {
 // TODO：✅==========================================================
 // 后序
 // 543. 二叉树的直径
-var diameterOfBinaryTree = function(root) {
+var no543_diameterOfBinaryTree = function(root) {
   let self_max = 0;
   if(root == null || (root.left == null && root.right == null)) return 0
   function dfs(root) {
@@ -170,6 +147,185 @@ var diameterOfBinaryTree = function(root) {
   }
   dfs(root)
   return self_max -1 ;
+};
+
+// TODO：✅==========================================================
+// HINT：传统递归理论可以，但是题目要求原地改动，所有采用分解问题
+// 114. 二叉树展开为链表
+// 遍历法
+var no114_flatten = function(root) {
+  const helper = (root) => {       // 将当前子树转成一个单链表
+    if (root == null) return null; // 遍历到null节点 返回null节点
+    if (root.right) {              // 先生成右子树的单链表
+      helper(root.right);
+    }
+    if (root.left) {               // 如果有左子树，生成单链表然后搬运过去
+      const leftFirst = helper(root.left); // 生成单链表，并获取头结点
+      let leftLast = leftFirst;    // leftLast是单链表的尾节点
+      while (leftLast.right) {     // 一直找右节点，获取到单链表的尾节点
+        leftLast = leftLast.right;
+      }
+      leftLast.right = root.right; // 尾节点后面接左子树展平后的单链表
+      root.right = leftFirst;      // 根节点的right改成leftFirst
+      root.left = null;            // root.left置为null
+    }
+    return root; // 返回出当前子树转成的单链表
+  };
+  helper(root);  // 原地修改，不用返回
+};
+
+function no114_flatten_dfs(root) {
+    if(!root) return null
+
+    no114_flatten_dfs(root.left)
+    no114_flatten_dfs(root.right)
+
+    let left = root.left
+    let right = root.right
+
+    root.left = null
+    root.right = left
+
+    while(root.right) {
+      root = root.right
+    }
+    root.right = right;
+}
+
+
+// TODO：❗️看了题解==========================================================
+// 116. 填充每个节点的下一个右侧节点指针
+// 给定一个 完美二叉树 ，其所有叶子节点都在同一层，每个父节点都有两个子节点。
+// 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+// 输入：root = [1,2,3,4,5,6,7]
+// 输出：[1,#,2,3,#,4,5,6,7,#]
+
+// HINT：递归，一棵二叉树被抽象成了一棵三叉树，三叉树上的每个节点就是原先二叉树的两个相邻节点。
+
+var no116_connect = function(root) {
+  if(!root) return []
+  function dfs(node1, node2) {
+    if(!node1 || !node2) return null
+    node1.next = node2
+
+    dfs(node1.left, node1.right)
+    dfs(node1.right, node2.left)
+    dfs(node2.left, node2.right)
+  }
+  dfs(root.left, root.right);
+  return root;
+};
+
+//226.翻转二叉树
+var no226_invertTree = function(root) {
+  function dfs(root) {
+      if(!root) return null;
+      let temp = root.right
+      root.right = root.left;
+      root.left = temp;
+      dfs(root.left)
+      dfs(root.right)
+      return root
+  }
+  dfs(root);
+
+  return root
+};
+
+function dfs( left, right) {
+  if(left > right) return null
+  let mid = (right + left) >>> 1; // 相当于除以2
+  let root = new TreeNode(nums[mid]) 
+  root.left = dfs(left, mid - 1)
+  root.right = dfs(mid + 1, right)
+
+  return root;
+}
+
+return dfs(0, nums.length - 1)
+
+// TODO：✅==========================================================
+// 我日：我自己做出来了 优化完：执行用时：94.34%，内存94.70%
+//654. 最大二叉树 [3,2,1,6,0,5]
+function no654_constructMaximumBinaryTree(nums) {
+  function dfs(nums) {
+    if(nums.length === 0) return null
+    let maxNum = Number.MIN_SAFE_INTEGER
+    let maxIndex = 0
+    for (let i = 0; i <= nums.length; i++) {
+      if (maxNum < nums[i]) {
+          maxIndex = i;
+          maxNum = nums[i];
+      }
+    }
+    let root = new TreeNode(nums[maxIndex])
+    root.left = dfs(nums.slice(0, maxIndex))
+    root.right = dfs(nums.slice(maxIndex+1))
+    return root
+  }
+
+  return dfs(nums)
+}
+
+// TODO：✅==========================================================
+// 105. 从前序与中序遍历序列构造二叉树
+TreeNode build(int[] preorder, int preStart, int preEnd, 
+  int[] inorder, int inStart, int inEnd) {
+if (preStart > preEnd) {
+return null;
+}
+
+// root 节点对应的值就是前序遍历数组的第一个元素
+int rootVal = preorder[preStart];
+// rootVal 在中序遍历数组中的索引
+int index = valToIndex.get(rootVal);
+
+int leftSize = index - inStart;
+
+// 先构造出当前根节点
+TreeNode root = new TreeNode(rootVal);
+// 递归构造左右子树
+root.left = build(preorder, preStart + 1, preStart + leftSize,
+         inorder, inStart, index - 1);
+
+root.right = build(preorder, preStart + leftSize + 1, preEnd,
+          inorder, index + 1, inEnd);
+return root;
+}
+
+// TODO：✅==========================================================
+// 105. 从前序与中序遍历序列构造二叉树
+
+function no105_buildTree(preorder, inorder) {
+  function dfs (preorder, inorder) {
+    if(!inorder.length) return null
+    let rootVal = preorder[0]
+    const root = new TreeNode(rootVal)
+    let inRootIndex = inorder.indexOf(rootVal)
+    root.left = dfs(preorder.slice(1, inRootIndex+1), inorder.slice(0, inRootIndex))
+    root.right = dfs(preorder.slice(inRootIndex+1), inorder.slice(inRootIndex+1))
+    return root
+  }
+
+  return dfs(preorder, inorder)
+};
+
+// 106. 从后序与中序遍历序列构造二叉树
+// 注意画图
+var no106_buildTree = function(inorder, postorder) {
+  function dfs (inS, inE, postS, postE) {
+    if(inS > inE) return null
+    let rootVal = postorder[postE]
+    let mid = inorder.indexOf(rootVal)
+    const root = new TreeNode(rootVal)
+    let leftSize = mid - inS
+    root.left = dfs(inS, mid - 1, postS, postS + leftSize - 1)
+    root.right = dfs(mid + 1, inE, postS + leftSize, postE - 1)
+
+    return root
+  }
+
+  return dfs(0, inorder.length - 1, 0, postorder.length - 1)
 };
 
 // TODO：❗️==========================================================
@@ -324,6 +480,21 @@ var no102_zigzagLevelOrder = function(root) {
   return res;
 };
 
+
+var no102_levelOrder = function(root) {
+  const res = [];
+  if(!root) return [];
+  function dfs(root, step) {
+    if(root) {
+      if(!res[step]) res[step] = []
+      res[step].push(root.val)
+      dfs(root.left, step+1 )
+      dfs(root.right, step+1)
+    }
+  }
+  dfs(root, 0);
+  return res;
+};
 
 // TODO：✅==========================================================
 // 103. 二叉树的锯齿形层序遍历
